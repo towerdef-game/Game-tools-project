4b8a56769887b67dac6fd69a56332bfd9e9e4474
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-04-28 20:24:29 +0100

    city generation
    implamented a city generation using perlin noise

---
+++ tools_project/Assets/GridSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+
+    public int BulidngsongridX = 4;
+    public int BuildingsongridZ = 4;
+    public GameObject prefabToSpawn;
+    public Vector3 gridOrigin = Vector3.zero;
+    public float Distancebetweenbuildings = 2f;
+  //  public bool generateOnEnable;
+
+
+    void Start()
+    {
+       // if (generateOnEnable)
+       // {
+            Generate();
+        //}
+    }
+
+    public void Generate()
+    {
+        SpawnGrid();
+    }
+
+
+    void SpawnGrid()
+    {
+        for (int x = 0; x < BulidngsongridX; x++)
+        {
+            for (int z = 0; z < BuildingsongridZ; z++)
+            {
+                GameObject clone = Instantiate(prefabToSpawn, 
+                    transform.position + gridOrigin + new Vector3(Distancebetweenbuildings * x, 0, Distancebetweenbuildings * z), transform.rotation);
+                clone.transform.SetParent(this.transform);
+            }
+        }
+    }
+
+    
+}
